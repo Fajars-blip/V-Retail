e57d7b0dbab3eb14599c6cca826880ec2a4338ff
Fajars-blip <ahmadfajarsidik123@gmail.com>
2026-05-10 22:32:49 +0700

    chore: update vite config and dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Menambahkan base agar path CSS/JS tidak error saat deploy ke GitHub Pages
   plugins: [vue()],
 })
