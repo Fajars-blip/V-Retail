a10dbe24f0fb24bbbd0a96eb32e115cbeb40fc6b
Fajars-blip <ahmadfajarsidik123@gmail.com>
2026-05-12 08:29:50 +0700

    fix: remove leading slash in index.html

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import AuthView from '../views/AuthView.vue'
 import DashboardLayout from '../layouts/DashboardLayout.vue'
@@ -47,7 +47,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/V-Retail/'),
   routes
 })
 
